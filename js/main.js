03f5c39d86e769b237efa783a9ab4a8bcf3d93ed
Grizly <burichin7@gmail.com>
2020-05-09 01:56:25 +0600

    day 4

--- js/main.js
+++ js/main.js
@@ -20,9 +20,16 @@ const restaurantTitle = document.querySelector('.restaurant-title');
 const rating = document.querySelector('.rating');
 const minPrice = document.querySelector('.price');
 const category = document.querySelector('.category');
+const modalBody = document.querySelector('.modal-body');
+const modalPrice = document.querySelector('.modal-pricetag');
+const buttonClearCart = document.querySelector('.clear-cart');
+
+
 
 
 let login = localStorage.getItem('Delivery');
+
+const cart = [];
 
 const getData = async function (url) {
 
@@ -59,6 +66,7 @@ function authorozed() {
     buttonAuth.style.display = '';
     userName.style.display = '';
     buttonOut.style.display = '';
+    cartButton.style.display = '';
     buttonOut.removeEventListener('click', logOut);
     checkAuth();
     returnMain();
@@ -70,7 +78,8 @@ function authorozed() {
 
   buttonAuth.style.display = 'none';
   userName.style.display = 'inline';
-  buttonOut.style.display = 'block';
+  buttonOut.style.display = 'flex';
+  cartButton.style.display = 'flex';
 
   buttonOut.addEventListener('click', logOut)
 };
@@ -178,11 +187,11 @@ function createCardGood(goods) {
 								<div class="ingredients">${description}</div>
 							</div>
 							<div class="card-buttons">
-								<button class="button button-primary button-add-cart">
+								<button class="button button-primary button-add-cart" id="${id}">
 									<span class="button-card-text">В корзину</span>
 									<span class="button-cart-svg"></span>
 								</button>
-								<strong class="card-price-bold">${price} ₽</strong>
+								<strong class="card-price card-price-bold">${price} ₽</strong>
 							</div>
 						</div>
   `);
@@ -230,7 +239,91 @@ function returnMain() {
   menu.classList.add('hide')
 };
 
+function addToCart(event) {
 
+  const target = event.target;
+  const buttonAddToCart = target.closest('.button-add-cart');
+
+  if (buttonAddToCart) {
+
+    const card = target.closest('.card');
+    const title = card.querySelector('.card-title-reg').textContent;
+    const cost = card.querySelector('.card-price').textContent;
+    const id = buttonAddToCart.id;
+
+    const food = cart.find(function (item) {
+      return item.id === id;
+    })
+
+    if (food) {
+      food.count += 1;
+    } else {
+      cart.push({
+        id,
+        title,
+        cost,
+        count: 1
+      });
+    }
+
+  }
+
+}
+
+function renderCart() {
+  modalBody.textContent = '';
+
+  cart.forEach(function ({
+    id,
+    title,
+    cost,
+    count
+  }) {
+    const itemCart = `
+        <div class="modal-body">
+				  <div class="food-row">
+					  <span class="food-name">${title}</span>
+					  <strong class="food-price">${cost}</strong>
+					<div class="food-counter">
+						<button class="counter-button counter-minus" data-id=${id}>-</button>
+						<span class="counter">${count}</span>
+						<button class="counter-button counter-plus" data-id=${id}>+</button>
+					</div>
+				</div>
+
+`;
+
+    modalBody.insertAdjacentHTML('afterbegin', itemCart);
+
+
+  });
+
+  const totalPrice = cart.reduce(function (result, item) {
+    return result + (parseFloat(item.cost) * item.count);
+  }, 0)
+
+  modalPrice.textContent = totalPrice + ' ₽';
+
+}
+
+function changeCount(event) {
+  const target = event.target;
+
+  if (target.classList.contains('counter-button')) {
+    const food = cart.find(function (item) {
+      return item.id === target.dataset.id;
+    });
+    if (target.classList.contains('counter-minus')) {
+      food.count--;
+      if (food.count === 0) {
+        cart.splice(cart.indexOf(food), 1);
+      }
+    }
+    if (target.classList.contains('counter-plus')) food.count++;
+    renderCart();
+  }
+
+}
 
 function init() {
   getData('./db/partners.json').then(function (data) {
@@ -239,7 +332,19 @@ function init() {
 
   });
 
-  cartButton.addEventListener("click", toggleModal);
+  cartButton.addEventListener("click", function () {
+    renderCart();
+    toggleModal();
+  });
+
+  buttonClearCart.addEventListener('click', function () {
+    cart.length = 0;
+    renderCart();
+  })
+
+  modalBody.addEventListener('click', changeCount);
+
+  cardsMenu.addEventListener("click", addToCart);
 
   close.addEventListener("click", toggleModal);
 
